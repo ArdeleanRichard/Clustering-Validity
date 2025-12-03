402cbe4879a642a1ee5a8097d6b2a77c6e0d25af
eardelean <E.Ardelean@stratec.com>
2025-12-03 14:49:40 +0200

    fix

--- metrics/DBCV_orig.py
+++ metrics/DBCV_orig.py
@@ -52,13 +52,66 @@ def get_subarray(
     return arr[inds_a_mesh, inds_b_mesh].T
 
 
+
+def prim_mst(
+    graph: npt.NDArray[np.float32], ind_root: int = 0
+) -> npt.NDArray[np.float32]:
+    n = len(graph)
+    intree = np.full(n, fill_value=False)
+    d = np.full(n, fill_value=np.inf)
+
+    d[ind_root] = 0
+    v = ind_root
+    counter = 0
+
+    G = {
+        "MST_edges": {
+            "node_inds": np.zeros((n - 1, 2), dtype=int),
+            "weights": np.zeros(n - 1, dtype=float),
+        },
+        "MST_degrees": np.zeros(n, dtype=int),
+        "MST_parent": np.arange(n),
+    }
+
+    while counter < n - 1:
+        intree[v] = True
+        dist = np.inf
+
+        for w in np.arange(n):
+            if w != v and not intree[w]:
+                weight = graph[v, w]
+
+                if d[w] > weight:
+                    d[w] = weight
+                    G["MST_parent"][w] = v
+
+                if dist > d[w]:
+                    dist = d[w]
+                    next_v = w
+
+        counter += 1
+        G["MST_edges"]["node_inds"][counter - 1, :] = (G["MST_parent"][next_v], next_v)
+        G["MST_edges"]["weights"][counter - 1] = graph[G["MST_parent"][next_v], next_v]
+        G["MST_degrees"][G["MST_parent"][next_v]] += 1
+        G["MST_degrees"][next_v] += 1
+        v = next_v
+
+    (inds_a, inds_b) = G["MST_edges"]["node_inds"].T
+    weights = G["MST_edges"]["weights"]
+
+    mst = np.zeros_like(graph)
+    mst[inds_a, inds_b] = weights
+    mst[inds_b, inds_a] = weights
+
+    return mst
+
 def get_internal_objects(
     mutual_reach_dists: npt.NDArray[np.float64], use_original_mst_implementation: bool
 ) -> npt.NDArray[np.float64]:
     if use_original_mst_implementation:
         mutual_reach_dists = np.copy(mutual_reach_dists)
         np.fill_diagonal(mutual_reach_dists, 0.0)
-        mst = reference_prim_mst.prim_mst(mutual_reach_dists)
+        mst = prim_mst(mutual_reach_dists)
 
     else:
         mst = scipy.sparse.csgraph.minimum_spanning_tree(mutual_reach_dists)
@@ -353,66 +406,3 @@ def dbcv(
     dbcv = float(np.sum(vcs * cluster_sizes)) / n
 
     return dbcv
-
-
-
-
-
-
-
-
-def prim_mst(
-    graph: npt.NDArray[np.float32], ind_root: int = 0
-) -> npt.NDArray[np.float32]:
-    """Python translation of the original implementation of Prim's MST in MATLAB.
-
-    Reference source: https://github.com/pajaskowiak/dbcv/blob/main/src/MST_Edges.m
-    """
-    n = len(graph)
-    intree = np.full(n, fill_value=False)
-    d = np.full(n, fill_value=np.inf)
-
-    d[ind_root] = 0
-    v = ind_root
-    counter = 0
-
-    G = {
-        "MST_edges": {
-            "node_inds": np.zeros((n - 1, 2), dtype=int),
-            "weights": np.zeros(n - 1, dtype=float),
-        },
-        "MST_degrees": np.zeros(n, dtype=int),
-        "MST_parent": np.arange(n),
-    }
-
-    while counter < n - 1:
-        intree[v] = True
-        dist = np.inf
-
-        for w in np.arange(n):
-            if w != v and not intree[w]:
-                weight = graph[v, w]
-
-                if d[w] > weight:
-                    d[w] = weight
-                    G["MST_parent"][w] = v
-
-                if dist > d[w]:
-                    dist = d[w]
-                    next_v = w
-
-        counter += 1
-        G["MST_edges"]["node_inds"][counter - 1, :] = (G["MST_parent"][next_v], next_v)
-        G["MST_edges"]["weights"][counter - 1] = graph[G["MST_parent"][next_v], next_v]
-        G["MST_degrees"][G["MST_parent"][next_v]] += 1
-        G["MST_degrees"][next_v] += 1
-        v = next_v
-
-    (inds_a, inds_b) = G["MST_edges"]["node_inds"].T
-    weights = G["MST_edges"]["weights"]
-
-    mst = np.zeros_like(graph)
-    mst[inds_a, inds_b] = weights
-    mst[inds_b, inds_a] = weights
-
-    return mst
